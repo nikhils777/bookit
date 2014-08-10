348dbdd32d25737dfdde4909719ebc3574faed3c
nikhils777 <nsharmatennis@gmail.com>
2014-08-10 18:04:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookit::Application.config.secret_key_base = '10177cbe6c38858395b4c2bcb0bfa4c805ea945a0245e4596fb942f378bdd1d0283d0f325da85036bb437ed6a747c5fa171012a66401afeeaf5e8e799d9c799d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookit::Application.config.session_store :cookie_store, key: '_bookit_session'
