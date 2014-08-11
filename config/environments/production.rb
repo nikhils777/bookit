4738c026477acafe4b823cf1d55c92d7ab88b604
nikhils777 <nsharmatennis@gmail.com>
2014-08-11 18:54:31 -0400

    added users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Bookit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: 'http://powerful-savannah-5221.herokuapp.com' }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Bookit::Application.routes.draw do
+  devise_for :users
   get "welcome/index"
   root "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
